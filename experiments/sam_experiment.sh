c97b93d276e0969841cbd4943fb1e0f10ab8f423
joncrall <erotemic@gmail.com>
2023-10-28 13:40:14 -0400

    wip

---
+++ experiments/sam_experiment.sh
@@ -0,0 +1,18 @@
+DVC_DATA_DPATH=$HOME/data/dvc-repos/shitspotter_dvc
+DVC_EXPT_DPATH=$HOME/data/dvc-repos/shitspotter_expt_dvc
+KWCOCO_BUNDLE_DPATH=$DVC_DATA_DPATH
+TRAIN_FPATH=$KWCOCO_BUNDLE_DPATH/train.kwcoco.zip
+VALI_FPATH=$KWCOCO_BUNDLE_DPATH/vali.kwcoco.zip
+
+sdvc request
+
+WATCH_DVC_EXPT_DPATH=$(geowatch_dvc --tags='phase2_expt' --hardware=auto)
+python -m watch.tasks.sam.predict \
+    --input_kwcoco "$KWCOCO_BUNDLE_DPATH"/train.kwcoco.zip \
+    --output_kwcoco "$KWCOCO_BUNDLE_DPATH"/train-sam.kwcoco.zip \
+    --fixed_resolution=None \
+    --weights_fpath "$WATCH_DVC_EXPT_DPATH/models/sam/sam_vit_h_4b8939.pth" \
+    --window_overlap=0.33333 \
+    --data_workers="2" \
+    --io_workers 0 \
+    --assets_dname="sam_feats"
