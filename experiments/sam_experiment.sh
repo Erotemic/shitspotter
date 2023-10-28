e94a291f374ce392bd7aad89f39dacc733b015cc
joncrall <erotemic@gmail.com>
2023-10-28 15:40:06 -0400

    wip

--- experiments/sam_experiment.sh
+++ experiments/sam_experiment.sh
@@ -4,13 +4,14 @@ KWCOCO_BUNDLE_DPATH=$DVC_DATA_DPATH
 TRAIN_FPATH=$KWCOCO_BUNDLE_DPATH/train.kwcoco.zip
 VALI_FPATH=$KWCOCO_BUNDLE_DPATH/vali.kwcoco.zip
 
-sdvc request
+#sdvc request "$WATCH_DVC_EXPT_DPATH/models/sam/sam_vit_h_4b8939.pth"
 
 WATCH_DVC_EXPT_DPATH=$(geowatch_dvc --tags='phase2_expt' --hardware=auto)
 python -m watch.tasks.sam.predict \
-    --input_kwcoco "$KWCOCO_BUNDLE_DPATH"/train.kwcoco.zip \
-    --output_kwcoco "$KWCOCO_BUNDLE_DPATH"/train-sam.kwcoco.zip \
+    --input_kwcoco "$KWCOCO_BUNDLE_DPATH"/vali.kwcoco.zip \
+    --output_kwcoco "$KWCOCO_BUNDLE_DPATH"/vali-sam.kwcoco.zip \
     --fixed_resolution=None \
+    --channels="red|green|blue" \
     --weights_fpath "$WATCH_DVC_EXPT_DPATH/models/sam/sam_vit_h_4b8939.pth" \
     --window_overlap=0.33333 \
     --data_workers="2" \
